52d617e16747675bf3b81b70afb677ac8b6596bd
volequyen <qvo9110@gmail.com>
2025-09-21 22:12:57 +0700

    done logout

--- src/api/getAllArticles.jsx
+++ src/api/getAllArticles.jsx
@@ -2,7 +2,6 @@ import { axiosPrivate } from "./axios";
 
 export const getAllArticles = async (pageUI = 1, size = 5) => {
   const backendPage = Math.max(0, pageUI - 1);
-
   const res = await axiosPrivate.get("/api/articles", {
     params: { page: backendPage } 
   });
