6d336ba5c52680c426e2b4f328955853183f174e
levy24 <vyle242004@gmail.com>
2025-11-13 23:41:29 +0700

    add Chatbot to Default Layout

--- src/layouts/DefaultLayout/index.jsx
+++ src/layouts/DefaultLayout/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import UserHeader from '../../components/UserHeader'
 import Footer from '../../components/Footer'
+import ChatbotWidget from '../../components/Chatbot/ChatbotWidget'
 
 function DefaultLayout() {
   return (
     <div>
         <UserHeader/>
+        <ChatbotWidget />
         <Outlet />
         <Footer/>
     </div>
