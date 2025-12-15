303892c82dba637e18057dc292467fe462284a04
volequyen <qvo9110@gmail.com>
2025-12-15 23:53:00 +0700

    fix header

--- src/layouts/DefaultLayout/index.jsx
+++ src/layouts/DefaultLayout/index.jsx
@@ -6,10 +6,14 @@ import ChatbotWidget from '../../components/Chatbot/ChatbotWidget'
 
 function DefaultLayout() {
   return (
-    <div>
-        <UserHeader/>
+    <div className="flex flex-col min-h-screen">
+        <div className="fixed top-0 left-0 right-0 z-50">
+            <UserHeader/>
+        </div>
         <ChatbotWidget />
-        <Outlet />
+        <div className="pt-24 grow bg-gray-50"> 
+            <Outlet />
+        </div>
         <Footer/>
     </div>
   )
