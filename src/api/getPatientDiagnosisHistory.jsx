76671731b5bc44a88fe3da7de2a0376d73ffda6e
volequyen <qvo9110@gmail.com>
2025-12-17 16:47:34 +0700

    fix fetchapi

--- src/api/getPatientDiagnosisHistory.jsx
+++ src/api/getPatientDiagnosisHistory.jsx
@@ -1,6 +1,6 @@
 import { axiosPrivate } from "./axios";
 
-export const getPatientDiagnosisHistory = async (userId) => {
+export const getPatientDiagnosisHistory = async () => {
   const url = `/api/user/diagnoses/by-user`;
   const response = await axiosPrivate.get(url);
   return response.data;
