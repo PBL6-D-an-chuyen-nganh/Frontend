0a8efe136542d188021e63a5a77258bb6fda158d
volequyen <qvo9110@gmail.com>
2025-12-21 18:09:25 +0700

    fix get id

--- src/pages/RoleDoctor/ScheduleRegister.jsx
+++ src/pages/RoleDoctor/ScheduleRegister.jsx
@@ -14,7 +14,7 @@ function ScheduleRegister() {
     const [selections, setSelections] = useState([]);
     const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState("");
-    const doctorId = useAuthStore((state) => state.user?.id);
+    const doctorId = useAuthStore((state) => state.user?.userId);
     console.log(doctorId);
 
     useEffect(() => {
